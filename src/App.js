061d2c683d2a6c3d2e55a954d6c79bf9759751b4
Razz67 <srasberry00@gmail.com>
2022-10-25 09:48:54 -0400

    Set up Routes for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React, { useEffect, useState } from "react";
+import { Route, Routes} from "react-router-dom"
+import Home from "./components/pages/Home";
+import SingleCoctail from "./components/pages/SingleCoctail";
+import  About from "./components/pages/About";
+
 
 export default function App() {
 	return (
 		<div className="App">
-		What's Up
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/:id" element={<SingleCoctail />} />
+			<Route path="/about" element={<About />} />
+		</Routes>
 		</div>
 	)
 }
